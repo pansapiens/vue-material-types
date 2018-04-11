95d7c99aae97325ee1042590d5b6f2fbd5c1434e
Andrew Perry <ajperry@pansapiens.com>
2018-04-12 09:58:53 +1000

    Tweaks, reformatting.

--- index.d.ts
+++ index.d.ts
@@ -1,91 +1,88 @@
 //Taken from https://github.com/vuematerial/vue-material/issues/662#issuecomment-308937275
 
-import {PluginFunction} from 'vue'
+import Vue from 'vue';
 
-type ExportProperty
-	= 'install'
-	| 'MdCore'
-	| 'MdAvatar'
-	| 'MdBackdrop'
-	| 'MdBottomBar'
-	| 'MdButton'
-	| 'MdButtonToggle'
-	| 'MdCard'
-	| 'MdCheckbox'
-	| 'MdChips'
-	| 'MdDialog'
-	| 'MdDivider'
-	| 'MdFile'
-	| 'MdIcon'
-	| 'MdImage'
-	| 'MdInputContainer'
-	| 'MdLayout'
-	| 'MdList'
-	| 'MdMenu'
-	| 'MdProgress'
-	| 'MdRadio'
-	| 'MdSelect'
-	| 'MdSidenav'
-	| 'MdSnackbar'
-	| 'MdSpeedDial'
-	| 'MdSpinner'
-	| 'MdSubheader'
-	| 'MdSwitch'
-	| 'MdTable'
-	| 'MdTabs'
-	| 'MdToolbar'
-	| 'MdTooltip'
-	| 'MdWhiteframe'
+type Component = 'MdCore'
+  | 'MdAvatar'
+  | 'MdBackdrop'
+  | 'MdBottomBar'
+  | 'MdButton'
+  | 'MdButtonToggle'
+  | 'MdCard'
+  | 'MdCheckbox'
+  | 'MdChips'
+  | 'MdDialog'
+  | 'MdDivider'
+  | 'MdFile'
+  | 'MdIcon'
+  | 'MdImage'
+  | 'MdInputContainer'
+  | 'MdLayout'
+  | 'MdList'
+  | 'MdMenu'
+  | 'MdProgress'
+  | 'MdRadio'
+  | 'MdSelect'
+  | 'MdSidenav'
+  | 'MdSnackbar'
+  | 'MdSpeedDial'
+  | 'MdSpinner'
+  | 'MdStep'
+  | 'MdSubheader'
+  | 'MdSwitch'
+  | 'MdTable'
+  | 'MdTabs'
+  | 'MdToolbar'
+  | 'MdTooltip'
+  | 'MdWhiteframe';
 
-type Options = {
-	[key in ExportProperty]: PluginFunction<never>
-}
+type Options =
+  { install: Vue.PluginFunction<never>} &
+  { [key in Component]: Vue.PluginFunction<never> };
 
-declare const options: Options
+declare const options: Options;
+export default options;
 
-export default options
-
-type Color
-	= 'red'
-	| 'pink'
-	| 'purple'
-	| 'deep-purple'
-	| 'indigo'
-	| 'blue'
-	| 'light-blue'
-	| 'cyan'
-	| 'teal'
-	| 'green'
-	| 'light-green'
-	| 'lime'
-	| 'yellow'
-	| 'amber'
-	| 'orange'
-	| 'deep-orange'
-	| 'brown'
-	| 'grey'
-	| 'blue-grey'
-	| 'white'
-	| 'black'
+type Color = 'red'
+| 'pink'
+| 'purple'
+| 'deep-purple'
+| 'indigo'
+| 'blue'
+| 'light-blue'
+| 'cyan'
+| 'teal'
+| 'green'
+| 'light-green'
+| 'lime'
+| 'yellow'
+| 'amber'
+| 'orange'
+| 'deep-orange'
+| 'brown'
+| 'grey'
+| 'blue-grey'
+| 'white'
+| 'black';
 
 type ThemeOption = Color | {
-	color?: Color
-	hue?: string | number
-	textColor?: Color
-}
+  color?: Color;
+  hue?: string | number;
+  textColor?: Color;
+};
 
 interface ThemeType {
-	primary?: ThemeOption
-	accent?: ThemeOption
-	warn?: ThemeOption
-	background?: ThemeOption
+  primary?: ThemeOption;
+  accent?: ThemeOption;
+  warn?: ThemeOption;
+  background?: ThemeOption;
 }
 
 declare module 'vue/types/vue' {
-	namespace Vue {
-		const material: {
-			registerTheme(name: string | { [key: string]: ThemeType }, spec?: ThemeType): void,
-			setCurrentTheme(name: string): void
-		}
-	}
+  namespace Vue {
+    const material: {
+      registerTheme(name: string | { [key: string]: ThemeType }, spec?: ThemeType): void,
+      setCurrentTheme(name: string): void
+    };
+  }
 }
